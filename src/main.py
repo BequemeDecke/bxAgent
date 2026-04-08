a4a6172ea2cf03f11f395a58db4e784474fac48c
BequemeDecke <lukas.drescher@hof-university.de>
2026-04-08 18:17:55 +0200

    Add a LocalShellBackend to let the agent execute shell commands

--- src/main.py
+++ src/main.py
@@ -5,8 +5,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 from pydantic import BaseModel, SecretStr, Field
 from langchain.chat_models import init_chat_model
-from langchain.messages import HumanMessage
+from langchain.messages import HumanMessage, SystemMessage
 from deepagents import create_deep_agent
+from deepagents.backends import LocalShellBackend
 
 # Load environment variables from the .env file
 dotenv_path = (
@@ -38,6 +39,7 @@ agent_config = BxAgentConfig(
 )
 
 
+# --- Builder Functions ---
 def build_chat_model():
     """Builds the chat model using the loaded configuration."""
     return init_chat_model(
@@ -48,23 +50,47 @@ def build_chat_model():
     )
 
 
-def build_bx_agent():
+def build_backend():
+    """
+    Builds the backend for the BxAgent.
+
+    When using the LocalShellBackend, we set virtual_mode to True to enable in-memory operations.
+    """
+    return LocalShellBackend(
+        root_dir=Path("./agent_data"), virtual_mode=True
+    )  # TODO: Make this configurable later on; Counter: 1
+
+
+def build_bx_agent(system_prompt: str):
     """Builds the BxAgent using the chat model."""
     model = build_chat_model()
-    return create_deep_agent(model=model)
+    backend = build_backend()
+
+    return create_deep_agent(
+        model=model, backend=backend, system_prompt=SystemMessage(system_prompt)
+    )
 
 
+# --- Main Execution ---
 def main():
     logging.info("Starting BxAgent with configuration: %s", agent_config)
 
-    bx_agent = build_bx_agent()
+    bx_agent = build_bx_agent(
+        system_prompt="You are a helpful assistant with coding capabilities in Python."
+    )
     logging.debug(f"BxAgent initialized successfully.")
 
     # TODO: Remove this example after testing
     response = bx_agent.invoke(
-        {"messages": [HumanMessage(content="Hello, how are you?")]}
+        {
+            "messages": [
+                HumanMessage(
+                    content="Write a Python function that adds two numbers and returns the result. The file should be named add.py and the function should be named add_numbers."
+                )
+            ]
+        },
     )
-    logging.info(f"Received response from chat model: {response}")
+    logging.info(f"Received response from bxAgent: {response}")
 
 
 if __name__ == "__main__":
