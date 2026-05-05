d1cd35eb789b52c1ecd875e931e9fb283a69776c
BequemeDecke <lukas.drescher@hof-university.de>
2026-05-05 08:32:33 +0200

    Extract env variables in config file

--- main.py
+++ main.py
@@ -15,46 +15,7 @@ from deepagents import create_deep_agent
 from deepagents.backends import LocalShellBackend, CompositeBackend, FilesystemBackend
 
 from prompt import SYSTEM_PROMPT
-
-# Load environment variables from the .env file
-dotenv_path = (
-    Path.cwd() / ".env"
-)  # Env file is located one level up from the src directory
-has_env_loaded = load_dotenv(dotenv_path=dotenv_path)
-assert has_env_loaded, f"Failed to load environment variables from {dotenv_path}"
-
-# Setup Logging
-logging.basicConfig(
-    level=logging.INFO,  # TODO: Let this be configurable; Counter: 2
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-)
-
-
-# Save the loaded environment variables to a config class for easy access
-class BxAgentConfig(BaseModel):
-    """Configuration class for BxAgent."""
-
-    API_KEY: SecretStr = Field()
-    BASE_URL: str = Field()
-    MODEL_ID: str = Field()
-
-
-agent_config = BxAgentConfig(
-    API_KEY=os.getenv("API_KEY"),
-    BASE_URL=os.getenv("BASE_URL"),
-    MODEL_ID=os.getenv("MODEL_ID"),
-)
-
-class LangFuseConfig(BaseModel):
-    SECRET_KEY: SecretStr = Field()
-    PUBLIC_KEY: SecretStr = Field()
-    BASE_URL: str = Field()
-    
-langfuse_config = LangFuseConfig(
-    SECRET_KEY=os.getenv("LANGFUSE_SECRET_KEY"),
-    PUBLIC_KEY=os.getenv("LANGFUSE_PUBLIC_KEY"),
-    BASE_URL=os.getenv("LANGFUSE_BASE_URL"),
-)
+from src.config import agent_config, langfuse_config
 
 
 # --- Builder Functions ---
