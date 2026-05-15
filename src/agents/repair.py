ca2e1b9b92d15c0080567ea5ec005b623ee40f37
BequemeDecke <lukas.drescher@hof-university.de>
2026-05-15 09:22:53 +0200

    Test repair agent

--- src/agents/repair.py
+++ src/agents/repair.py
@@ -7,9 +7,10 @@ from langgraph.checkpoint.memory import InMemorySaver
 
 from src.models import build_coding_model
 from src.tools.testing import check_file_existence, check_compilation
+from src.output.repair import RepairOutputSchema
 
 REPAIR_SYSTEM_PROMPT = """
-You are a subagent responsible for repairing code. 
+You are a subagent resp onsible for repairing code. 
 You will be given a code snippet and an error message. 
 Your task is to analyze the error message, understand the issue in the code, and provide a corrected version of the code snippet that resolves the error. 
 Make sure to explain the changes you made to fix the issue.
@@ -31,4 +32,5 @@ def build_repair_agent(
         checkpointer=InMemorySaver(),
         tools=[check_file_existence, check_compilation], # Exploration and editing tools are added by deepagents
         backend=backend,
+        # response_format=RepairOutputSchema
     )
