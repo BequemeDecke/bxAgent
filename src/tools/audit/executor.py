f16e2f31bb216eda4cccf62cc3a4b66653a29bb8
BequemeDecke <lukas.drescher@hof-university.de>
2026-05-22 09:04:22 +0200

    Implement execute_specific

--- src/tools/audit/executor.py
+++ src/tools/audit/executor.py
@@ -1,22 +1,60 @@
 import asyncio
+import datetime
 
-from typing import List
+from typing import List, Dict
 
-from .types import Audit, AuditRun
+from .types import Audit, AuditRun, AuditError
 
 
 class AuditExecutor:
     def __init__(self, audits: List[Audit]):
-        self.audits: List[Audit] = audits
+        self.audits: Dict[str, Audit] = {audit.audit_id: audit for audit in audits}
+        self.iterations: Dict[str, List[AuditRun]] = {
+            audit.audit_id: [] for audit in audits
+        }
 
     async def execute_all(self) -> List[AuditRun]:
         results = []
-        tasks = [audit.run() for audit in self.audits]
+        tasks = []
+
+        for audit in self.audits:
+            started_at = datetime.datetime.now()
+            tasks.append(audit.run())
+
         results = await asyncio.gather(*tasks)
         return results
-    
+
     async def execute_specific(self, audit_id: str) -> AuditRun:
-        for audit in self.audits:
-            if audit.audit_id == audit_id:
-                return await audit.run()
-        raise ValueError(f"Audit with id {audit_id} not found.")
+        if audit_id not in self.audits:
+            raise ValueError(f"Audit with id {audit_id} not found.")
+
+        audit = self.audits[audit_id]
+        started_at = datetime.datetime.now()
+        iteration = (
+            self.iterations[audit_id][-1].iteration + 1
+            if self.iterations[audit_id]
+            else 1
+        )
+
+        try:
+            run_tuple = await audit.run()
+        except Exception as e:
+            run_tuple = (
+                [],
+                [
+                    AuditError(
+                        message=str(e), details={"exception_type": type(e).__name__}
+                    )
+                ],
+            )
+        execution_time_ms = int(
+            (datetime.datetime.now() - started_at).total_seconds() * 1000
+        )
+
+        return AuditRun(
+            started_at=started_at,
+            execution_time_ms=execution_time_ms,
+            iteration=iteration,
+            results=run_tuple[0],
+            errors=run_tuple[1],
+        )
