2f796c0b711b3cdba3e76dfb550baba6f51a7bd3
BequemeDecke <lukas.drescher@hof-university.de>
2026-06-02 11:30:05 +0200

    Update synthesis agent state

--- bxagent/agents/synthesis/output.py
+++ bxagent/agents/synthesis/output.py
@@ -1,7 +1,7 @@
 """
 This module defines the output schema for the synthesis agent. It specifies the structure of the data that the synthesis agent will return after it has completed its task. The schema includes a list of written files, which are represented as PathLike objects. This allows for a standardized way to represent the output of the synthesis agent, making it easier to integrate with other components of the system.
 
-Some LLMs do not support response formats 
+Some LLMs do not support response formats
 "For models that don’t support native structured output, LangChain uses tool calling to achieve the same result."
 
 Following these guides:
@@ -9,20 +9,10 @@ https://docs.langchain.com/oss/python/langchain/structured-output
 https://docs.langchain.com/oss/python/deepagents/customization#structured-output
 """
 
-
 from pydantic import BaseModel, Field
-from pathlib import Path
 
 
 class SynthesisResponseFormat(BaseModel):
-    """
-    Schema for the output of the synthesis agent.
-    """
-
-    # TODO: This is optinal for later
-    # is_transformation_possible: bool = Field(
-    #     description="Indicates whether the transformation is possible."
-    # )
-    # reason: str = Field(description="The reason for the transformation result.")
-    written_files: list[Path] = Field(description="The list of files that have been written by the synthesis agent.")
-
+    implementation_instructions: str = Field(
+        description="The implementation instructions for the transformation class. This should include a detailed description of how to implement the transformation class, including any important details or considerations that should be taken into account during implementation."
+    )
