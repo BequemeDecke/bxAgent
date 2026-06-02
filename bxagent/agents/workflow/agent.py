6218883fe3e20e5ed24d2a7afbc28f80a37e41bf
BequemeDecke <lukas.drescher@hof-university.de>
2026-06-02 10:41:37 +0200

    Add transformation iteration check as conditional edge to graph builder

--- bxagent/agents/workflow/agent.py
+++ bxagent/agents/workflow/agent.py
@@ -1,8 +1,24 @@
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, END
 
 from bxagent.models import build_base_model
+from .state import WorkflowState
+from .transformation_iteration_control import create_check_transformation_iteration_function
 
 def build_workflow_agent() -> StateGraph:
     llm = build_base_model()
+    check_transformation_iteration = create_check_transformation_iteration_function(llm)
     
-    pass
+    builder = StateGraph(WorkflowState)
+    
+    # TODO: Add the rest of the workflow nodes and edges!
+    builder.add_conditional_edges(
+        "check_transformation_iteration",
+        check_transformation_iteration,
+        {
+            "stop": END,
+            "continue": "continue_node",
+            "error": END,
+        },
+    )
+    
+    return builder
