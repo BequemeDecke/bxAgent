9defed091cc118e6f78c6ee619cccb90849c3488
BequemeDecke <lukas.drescher@hof-university.de>
2026-06-09 10:08:12 +0200

    Create transformation.plan module for better handling of the TRANSFORMATION.md

---
+++ tests/tools/transformation/plan_test.py
@@ -0,0 +1,33 @@
+"""
+The TRANSFORMATION.md file is a crucial component and encapsulates the information about the transformation.
+
+The file now should have a template structure that lets the agent simply fill in or retrieve the necessary information for the transformation.
+"""
+
+from unittest import TestCase
+
+
+class TestTransformationPlan(TestCase):
+    def test_transformation_plan__create_plan(self):
+        self.assertTrue(False)
+
+    def test_transformation_plan__read_whole_plan(self):
+        self.assertTrue(False)
+
+    def test_transformation_plan__update_package_information(self):
+        self.assertTrue(False)
+
+    def test_transformation_plan__update_iteration(self):
+        self.assertTrue(False)
+
+    def test_transformation_plan__update_model_implementation(self):
+        self.assertTrue(False)
+
+    def test_transformation_plan__update_transformation_direction(self):
+        self.assertTrue(False)
+
+    def test_transformation_plan__update_transformation_difficulties(self):
+        self.assertTrue(False)
+
+    def test_transformation_plan__update_implementation_steps(self):
+        self.assertTrue(False)
