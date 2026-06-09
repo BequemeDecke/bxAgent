096594876631b449d7c579e6de487d1f7544189f
BequemeDecke <lukas.drescher@hof-university.de>
2026-06-09 08:47:27 +0200

    Add failing tests to coding module

---
+++ tests/tools/coding/implement_bx_tool_test.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestImplementBxTool(TestCase):
+    def test_implement_bx_tool__return_compiled_code(self):
+        self.assertTrue(False)
---
+++ tests/tools/coding/implement_transformation_test.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestImplementTransformation(TestCase):
+    def test_implement_transformation__return_compiled_code(self):
+        self.assertTrue(False)
