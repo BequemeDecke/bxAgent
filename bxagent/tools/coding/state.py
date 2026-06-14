7c01ca2f4186249d31dd09240bf6ae9b8d508c62
BequemeDecke <lukas.drescher@hof-university.de>
2026-06-14 11:26:11 +0200

    Update state and files in coding module

--- bxagent/tools/coding/state.py
+++ bxagent/tools/coding/state.py
@@ -1,10 +1,13 @@
-from typing import TypedDict, List
+from typing import Dict, TypedDict, List
 from pathlib import Path
 
+from bxagent.tools.audit.types import AuditRun
 from bxagent.tools.transformation.plan import TransformationPlan
+
 
 class CodingAgentState(TypedDict):
     transformation_md: TransformationPlan
     task_specification: str  # This field will be used by a higher component
     written_java_files: List[Path]  # All of these files have to be compiled together
-    transformation_implementation: str # This field will be used by a higher component
+    transformation_implementation: str  # This field will be used by a higher component
+    latest_audit_results: Dict[str, AuditRun]  # Store the results of the latest audits
