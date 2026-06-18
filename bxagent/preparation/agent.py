cf3acf3a2cd4490f8d2c3944e2d09d7a170073fd
BequemeDecke <lukas.drescher@hof-university.de>
2026-06-18 07:41:10 +0200

    Build preparation agent

--- bxagent/preparation/agent.py
+++ bxagent/preparation/agent.py
@@ -6,6 +6,7 @@ from bxagent.agents.workflow.nodes.validation_node import (
     create_validation_agent_work_function,
 )
 
+from .explore_models import create_explore_models_node
 from .prepare_workspace import create_prepare_workspace_node
 
 
@@ -23,11 +24,14 @@ def build_preparation_agent(validation_executor: ValidationExecutor) -> StateGra
         },
         execution_mode="specific",
     )
+    explore_models_node = create_explore_models_node()
     prepare_workspace_node = create_prepare_workspace_node()
 
     graph = StateGraph()
     graph.add_node("prepare_workspace", prepare_workspace_node)
+    graph.add_node("explore_models", explore_models_node)
     graph.add_node("validate_preparation", validate_preparation_node)
     
-    graph.add_edge("prepare_workspace", "validate_preparation")
+    graph.add_edge("prepare_workspace", "explore_models")
+    graph.add_edge("explore_models", "validate_preparation")
     return graph
