a41d61b97d576ecb0ec30b2dca390be73f33b0c6
BequemeDecke <lukas.drescher@hof-university.de>
2026-09-12 12:09:28 +0200

    Move evaluation tools to evaluation module

--- mdeagent/tools/__init__.py
+++
@@ -1,3 +0,0 @@
-from .evaluation import create_evaluation_tools
-
-__all__ = ["create_evaluation_tools"]
--- tests/tools/evaluation_tool_test.py
+++ tests/tools/evaluation_tool_test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from langchain.tools import BaseTool
-from mdeagent.tools.evaluation import create_evaluation_tools
+from mdeagent.evaluation.tools import create_evaluation_tools
 
 
 class TestEvaluationToolFactory(unittest.TestCase):
